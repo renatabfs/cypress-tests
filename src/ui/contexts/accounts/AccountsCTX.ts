e79e7bcbc7b33c9f6430786119cd667c4adf7123
renata <renata.niall66@gmail.com>
2022-10-03 15:23:14 +0000

    create adicionado

--- src/ui/contexts/accounts/AccountsCTX.ts
+++ src/ui/contexts/accounts/AccountsCTX.ts
@@ -4,6 +4,7 @@ import Account from "../../../data/core/domain/models/Acount";
 interface Props {
     data?: Array<Account>
     fetch(): Promise <void>
+    create (account: Account): Promise<boolean>
 }
 
 export const AccountsCTX = createContext({} as Props);
