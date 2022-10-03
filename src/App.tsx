96a06012485516843bf3f7977992ce1362244db4
renata <renata.niall66@gmail.com>
2022-10-03 14:36:36 +0000

    created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import AccountAPI from "./data/api/AccountAPI"
 import AccountsProvider from "./ui/contexts/accounts/AccountsProvider"
-import ListAccountPages from "./ui/pages/accounts/ListAccountPages"
+import ListAccountPages from "./ui/pages/accounts/ListAccountPage/index"
+import CreateAccountsPage from "./ui/pages/accounts/CreateAccountPage"
 
 function App() {
 
   return (
     <AccountsProvider>
-      <ListAccountPages />
-      </AccountsProvider >
+    <BrowserRouter>
+    <Routes>
+    <Route path="/accounts" element={ <ListAccountPages />}/>
+    <Route path="/criar-conta" element= {<CreateAccountsPage />} />
+    </Routes>
+    </BrowserRouter>
+    </AccountsProvider >
+
   )
 }
 
