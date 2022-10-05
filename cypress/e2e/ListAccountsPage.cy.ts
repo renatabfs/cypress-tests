c4e3b8ff1f97edde64a7becab9a7def77b4012df
renatabfs <renata.niall66@gmail.com>
2022-10-05 08:26:57 -0300

    Teste de listar todas as contas na tabela criado

--- cypress/e2e/ListAccountsPage.cy.ts
+++ cypress/e2e/ListAccountsPage.cy.ts
@@ -1,16 +1,25 @@
-describe ("List Accounts Page", () => {
+describe("List Accounts Page", () => {
+    beforeEach(() => {
+        cy.intercept('GET', 'http://localhost:3000/accounts', {
+            fixture: 'accounts.json',
+        });
+    });
 
-it ('should have title, create account button and table', () => {
-    cy.intercept('GET', 'http://localhost:3000/accounts', {fixture: 'accounts.json'})
-    cy.visit('http://localhost:4173/')
-    cy.get('header h1').should('have.text', 'Lista de contas')
-    cy.get('header button').should('have.text', 'criar conta')
-    cy.get('table').should('exist')
-    cy.get('table thead tr th').should('have.length', 5)
-    cy.get('table thead tr th:nth-child(1)').should('have.text', 'ID')
-    cy.get('table thead tr th:nth-child(2)').should('have.text', 'Nome')
-    cy.get('table thead tr th:nth-child(3)').should('have.text', 'Data de nascimento')
-    cy.get('table thead tr th:nth-child(4)').should('have.text', 'e-mail')
-    cy.get('table thead tr th:nth-child(5)').should('have.text', 'CPF')
-})
+    it('should have title, create account button and table', () => {
+        cy.visit('http://localhost:4173/')
+        cy.get('header h1').should('have.text', 'Lista de contas')
+        cy.get('header button').should('have.text', 'criar conta')
+        cy.get('table').should('exist')
+        cy.get('table thead tr th').should('have.length', 5)
+        cy.get('table thead tr th:nth-child(1)').should('have.text', 'ID')
+        cy.get('table thead tr th:nth-child(2)').should('have.text', 'Nome')
+        cy.get('table thead tr th:nth-child(3)').should('have.text', 'Data de nascimento')
+        cy.get('table thead tr th:nth-child(4)').should('have.text', 'e-mail')
+        cy.get('table thead tr th:nth-child(5)').should('have.text', 'CPF')
+    })
+
+    it('should list accounts on table when fetching suceffuly', () => {
+        cy.get('table tbody tr').should('have.length', 2)
+        cy.get('table tbody tr:first-child td').should('have.length', 5)
+    })
 })
